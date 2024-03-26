292d52e154fa9e68c91533526bfcd031c50b1b7d
Abhitesh Pundir <103356440+AbhiteshPundir@users.noreply.github.com>
2024-03-26 21:16:21 +0530

    Updated Homepage.jsx

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,6 +1,6 @@
 function Homepage() {
   return (
-    <div className="bg-cover bg-opacity-55 bg-[url('D:\reacts\coinStalker\src\assets\bitcoinbg.jpg')]">
+    <div className="bg-cover bg-opacity-55 bg-[url('.\assets\bitcoinbg.jpg')]">
     <div className="bg-purple-800 bg-opacity-40 h-screen bg-cover flex flex-col align-middle items-end pr-14 justify-center text-white text-6xl ">
       <div className="font-extrabold tracking-wider"> Welcome to CoinStalker,<br/>
         <span>your own crypto-currency dashboard!!</span>
